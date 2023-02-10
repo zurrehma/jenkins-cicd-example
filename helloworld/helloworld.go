ac71e4d44d5339e510ee0861206c3484100f2ccd
zurrehma <zahid.chashma@gmail.com>
2023-02-11 00:10:49 +0500

    code for jenkins ci/cd

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -10,10 +10,15 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! Current URL path: %s", r.URL.Path)
 }
 
+func HelloEurope(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Europe! Current URL path: %s", r.URL.Path)
+}
+
 // main is the entry point which runs the Hello World server
 func main() {
 	// Route all requests to HelloWorld
 	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", HelloEurope)
 
 	// Listening on port 9094
 	http.ListenAndServe(":9094", nil)
